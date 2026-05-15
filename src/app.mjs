ea255f314d52ddeb410392af8c0960a9d1741912
MAFERCARRE <mafercarrenio@gmail.com>
2026-05-15 13:01:47 -0300

    Modifiqué archivo de servidor

--- src/app.mjs
+++ src/app.mjs
@@ -2,13 +2,8 @@ import express from 'express';// Importar el framework Express para crear el ser
 import path from 'path';// Importar el módulo 'path' para manejar rutas de archivos y directorios
 import expressLayouts from 'express-ejs-layouts';// Importar el middleware 'express-ejs-layouts' para usar layouts con EJS
 import { connectDB } from './config/dbConfig.mjs';// Importar la función 'connectDB' para conectar a la base de datos MongoDB
-//Guia para importar rutas- API y Frontend
-// Importación de rutas- API y Frontend
-// import paisesRoutes from './routes/superHeroesRoutes.mjs';
-// import paisesRoutefront from './routes/superHeroFront.mjs';
-//Nuevas rutas- API y Frontend
-import superHeroRoutes from './routes/paisesRoutes.mjs';// Importar las rutas de la API para paises
-import heroesroutefront from './routes/paisesFront.mjs';// Importar las rutas del frontend para paises
+//import paisesRoutes from './routes/paisesRoutes.mjs';// Importar las rutas de la API para paises
+import paisesfront from './routes/paisesFront.mjs';// Importar las rutas del frontend para paises
 
 const app = express();// Crear instancia de Express
 const PORT = process.env.PORT || 3000;// Definir el puerto para el servidor, usando una variable de entorno o el puerto 3000 por defecto
@@ -24,7 +19,7 @@ app.set('layout', 'layout'); // Busca views/layout.ejs- Indica que use layout.ej
 // Archivos estáticos y Middlewares
 app.use(express.static(path.resolve("./public")));// Servir archivos estáticos desde la carpeta "public" (para CSS, JS, imágenes, etc.)
 app.use(express.json()); // Para procesar JSON en el cuerpo de las peticiones
-//app.use(express.urlencoded({ extended: true })); // Útil si envías datos por formularios simples ????
+app.use(express.urlencoded({ extended: true })); // Útil si envías datos por formularios simples ????
 
 // Conexión a MongoDB
 connectDB();
@@ -38,11 +33,10 @@ app.get('/', (req, res) => {
 });
 
 // Rutas de la API (Backend)
-app.use('/api', superHeroRoutes);
+app.use('/api', paisesRoutes);// Configurar las rutas de la API para paises, que se encuentran en el archivo paisesRoutes.mjs
 
 // Rutas del Dashboard (Frontend/Vistas)
 //Guia para configurar rutas- API y Frontend
-//app.use("/dashboard", heroesroutefront);
 app.use("/dashboard", paisesfront);// Configurar la ruta para el dashboard, que renderiza las vistas del frontend para paises   
 
 // Manejo de errores 404 (Página no encontrada)
@@ -50,10 +44,6 @@ app.use((req, res) => {
     res.status(404).render('404', { title: 'Página no encontrada' });
 });
 
-// Iniciar el servidor
-// app.listen(PORT, () => {
-//     console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
 //Importante: Para levantar el servidor en render es necesario configurar el puerto http al que render va escuchar
 app.listen(PORT, '0.0.0.0', () => {//
     console.log(`Servidor levantado en el puerto ${PORT}`);
---
+++ src/routes/paisesRoutes.mjs
@@ -0,0 +1,49 @@
+//defino rutas de la API para paises
+import express from "express";
+import { getDashboardController } from "../controllers/paisesControllers.mjs"; 
+
+
+
+
+
+
+
+
+
+//Rutas fijas:
+// //TP3-S2
+// //Superheroes mayores a 30 años
+// //http://localhost:3000/api/heroes/mayores-30
+// router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
+// //Rutas con parámetros
+// //Superheroes por id
+// //http://localhost:3000/api/heroes/69c6fd59d90e243b1c0fad1b
+// router.get('/heroes/:id', obtenerSuperheroePorIdController);
+// //Superheroes por atributo
+// //http://localhost:3000/api/heroes/buscar/planetaOrigen/Tierra
+// router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
+
+// //
+// //Todos los superheroes
+// //GET- Mostrar todos los superheroes
+// // http://localhost:3000/api/heroes
+// router.get('/heroes', obtenerTodosLosSuperheroesController);
+
+// //POST- Crear un superheroe
+// //http://localhost:3000/api/heroes
+// router.post('/heroes', validateSuperHeroe, crearSuperHeroeController);
+
+// //PUT- Actualizar un superheroe por id
+// //http://localhost:3000/api/heroes/:id
+// router.put('/heroes/id/:id', validateSuperHeroe, actualizarSuperHeroeController);
+
+
+// //DELETE- Elimnar un superheroe por id
+// //http://localhost:3000/api/heroes/:id
+// //http://localhost:3000/api/heroes/69e00b5f98572b8f21c7876d
+// router.delete('/heroes/id/:id', eliminarSuperHeroeporIdController);
+
+// //DELETE- Elimnar un superheroe por nombre
+// router.delete('/heroes/nombre/:nombre', eliminarSuperHeroeporNombreController);
+
+// export default router;
