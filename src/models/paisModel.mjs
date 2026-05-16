92e5146c5f113f341376c67fd88579a787c33338
MAFERCARRE <mafercarrenio@gmail.com>
2026-05-16 17:33:53 -0300

    Modifique Modelo

--- src/models/paisModel.mjs
+++ src/models/paisModel.mjs
@@ -1,71 +1,60 @@
-// models/Paises.js
-import mongoose from 'mongoose';
+import mongoose from 'mongoose';// Importamos Mongoose para definir el esquema y el modelo de datos
 
 const paisSchema = new mongoose.Schema({
-    // Nombre oficial: 3-90 caracteres, obligatorio, sin espacios al inicio o final
-    nombreOficial: {
-        type: String,
+    nombreOficial: { 
+        type: String, 
         required: [true, 'El nombre oficial es obligatorio'],
-        trim: true,// Elimina espacios al inicio y al final
+        trim: true,
         minlength: [3, 'Mínimo 3 caracteres'],
         maxlength: [90, 'Máximo 90 caracteres']
     },
-    // Capital: Es un array de strings (ya que un país puede tener varias)
     capital: {
         type: [String],
         default: ['No tiene']
     },
-    // Fronteras/Limites: Array de códigos de 3 letras (Ej: ARG, BRA, CHL)-Equivale a los países limitrofes 
-    borders: {
+    borders: {// La API devuelve un array de códigos de países limítrofes, si no tiene, dejamos un array vacío
         type: [String],
         default: []
     },
-    // Área: Número no negativo     
     area: {
         type: Number,
         min: [0, 'El área no puede ser negativa'],
         default: 0
     },
-    // Población: Número no negativo
     population: {
         type: Number,
         min: [0, 'La población no puede ser negativa'],
         default: 0
     },
-    // Opcion Avanzada -Gini: (null por defecto)- Gini: Coeficiente de desigualdad, número entre 0 y 100
     gini: {
         type: Number,
         default: null
     },
-    // Zonas horarias: Array de strings
     timezones: {
         type: [String],
         default: []
     },
-    // Bandera: Guardaremos la URL de la imagen SVG
     bandera: {
         type: String,
         required: [true, 'La URL de la bandera es obligatoria']
     },
-    // Creador: Para identificar quién creó el país, con un valor por defecto.
     creador: {
         type: String,
-        default: "Fernanda",// Para que identifique los paises creados por mi, pero se puede cambiar al crear un nuevo país
-        immutable: true // Evita que se cambie accidentalmente
+        default: "Fernanda",
+        immutable: true
     },
-    // TipoDocumento: Para identificar que el documento es un país, con un valor por defecto. Porque estamos usando una colección compartida (Grupo-04),
-    //  entonces esto nos ayuda a filtrar solo los documentos que son países.
-    tipoDocumento: { type: String, default: "pais" ,immutable: true },// Para que nunca cambie}, 
-    timestamps: true, // Crea automáticamente 'createdAt' y 'updatedAt' para saber cuándo se creó y actualizó cada país
-    versionKey: false // Quita el campo __v que pone Mongoose por defecto
+    tipoDocumento: { // Campo para discriminar el tipo de documento, en este caso "pais" porque usamos una colección compartida (Grupo-04)
+        type: String, 
+        default: "pais",
+        immutable: true 
+    }
+}, { 
+    // Agregamos opciones al esquema para que Mongoose maneje automáticamente los campos de fecha de creación y actualización, 
+    // y para no incluir el campo __v que Mongoose agrega por defecto para el control de versiones.
+    timestamps: true, 
+    versionKey: false 
 });
 
-// Exportamos el modelo
-// Cambiamos esto porque no tenemos permitidos agregar nueva colección, entonces le decimos a Mongoose que use la colección "Grupo-04" 
-// que ya existe en la base de datos.:  
-// const Pais = mongoose.model('Pais', paisSchema);
-
-// Por esto (agregando el tercer parámetro con el nombre exacto de la colección):
-const Pais = mongoose.model('Pais', paisSchema, 'Grupo-04'); 
-
+const Pais = mongoose.model('Pais', paisSchema, 'Grupo-04'); // El tercer parámetro es el nombre de la colección en MongoDB. 
+// Usamos "Grupo-04" porque es una colección compartida.
 export default Pais;
