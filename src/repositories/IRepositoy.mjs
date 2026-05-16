44d5131206a36f3795b70382c3bd0dad29f42f96
MAFERCARRE <mafercarrenio@gmail.com>
2026-05-16 11:34:27 -0300

    Agregué metodo en IRepository para guardar registro de la APi externa

--- src/repositories/IRepositoy.mjs
+++ src/repositories/IRepositoy.mjs
@@ -1,19 +1,29 @@
-// src/repositories/IRepository.mjs
 class IRepository {
     obtenerTodos() {
         throw new Error("Método 'obtenerTodos()' no implementado");
     }
+
+    // Nuevo método para guardar múltiples registros (Para tomar datos de la API externa)
+    guardarMuchos(datos) {
+        throw new Error("Método 'guardarMuchos()' no implementado");
+    }
+
     obtenerPorId(id) {
         throw new Error("Método 'obtenerPorId()' no implementado");
     }
+
     crear(datos) {
         throw new Error("Método 'crear()' no implementado");
     }
+
     actualizar(id, datos) {
         throw new Error("Método 'actualizar()' no implementado");
     }
+
     eliminar(id) {
         throw new Error("Método 'eliminar()' no implementado");
     }
 }
+
 export default IRepository;
+
