240bb32d7af8b9b5ea658b08a90146485df77c9d
MAFERCARRE <mafercarrenio@gmail.com>
2026-05-22 23:46:29 -0300

    Importe funciones de validación en archivo de rutas: paisesRoutes

--- src/routes/paisesRoutes.mjs
+++ src/routes/paisesRoutes.mjs
@@ -1,5 +1,8 @@
 //defino rutas de la API para paises
 import express from 'express';// Importar el framework Express para crear el servidor web
+import { validatePais } from '../middlewares/middlewarePais.mjs';// Importar el middleware de validación para los países    
+import {body, validationResult} from 'express-validator';// Importar funciones de express-validator para validar los datos de entrada
+
 import { obtenerYProcesarPaises, crearPais, actualizarPais, obtenerPaisPorId, eliminarPaisporId } from '../services/paisService.mjs';// Importar el servicio que obtiene y procesa los datos de la API externa
 import paisRepository from '../repositories/paisRepository.mjs';// Importar el repositorio que guarda los datos en la base de datos
 const router = express.Router();
@@ -18,8 +21,8 @@ router.get('/sincronizar', async (req, res) => {
 });
 
 // Ruta para agregar un nuevo país (desde el formulario del frontend)
-//router.post('/paises', validatePais, crearPaisController);
-router.post('/paises', crearPaisController);
+router.post('/paises', validatePais, crearPaisController);
+//router.post('/paises', crearPaisController);
 
 // Ruta para editar un pais existente (desde el formulario del frontend)
 //router.put('/paises/id/:id', validateSuperHeroe, actualizarSuperHeroeController);
@@ -32,46 +35,3 @@ router.get('/paises/:id', obtenerPaisPorIdController);
 //ruta para eliminar un país por ID (desde el botón de eliminar en el frontend)
 router.delete('/paises/id/:id', eliminarPaisporIdController);
 export default router;
-
-
-
-
-
-
-//Rutas fijas:
-// //TP3-S2
-// //Superheroes mayores a 30 años
-// //http://localhost:3000/api/heroes/mayores-30
-// router.get('/heroes/mayores-30', obtenerSuperheroesMayoresDe30Controller);
-// //Rutas con parámetros
-// //Superheroes por id
-// //http://localhost:3000/api/heroes/69c6fd59d90e243b1c0fad1b
-// router.get('/heroes/:id', obtenerSuperheroePorIdController);
-// //Superheroes por atributo
-// //http://localhost:3000/api/heroes/buscar/planetaOrigen/Tierra
-// router.get('/heroes/buscar/:atributo/:valor', buscarSuperheroesPorAtributoController);
-
-// //
-// //Todos los superheroes
-// //GET- Mostrar todos los superheroes
-// // http://localhost:3000/api/heroes
-// router.get('/heroes', obtenerTodosLosSuperheroesController);
-
-// //POST- Crear un superheroe
-// //http://localhost:3000/api/heroes
-// router.post('/heroes', validateSuperHeroe, crearSuperHeroeController);
-
-// //PUT- Actualizar un superheroe por id
-// //http://localhost:3000/api/heroes/:id
-// router.put('/heroes/id/:id', validateSuperHeroe, actualizarSuperHeroeController);
-
-
-// //DELETE- Elimnar un superheroe por id
-// //http://localhost:3000/api/heroes/:id
-// //http://localhost:3000/api/heroes/69e00b5f98572b8f21c7876d
-// router.delete('/heroes/id/:id', eliminarSuperHeroeporIdController);
-
-// //DELETE- Elimnar un superheroe por nombre
-// router.delete('/heroes/nombre/:nombre', eliminarSuperHeroeporNombreController);
-
-// export default router;
