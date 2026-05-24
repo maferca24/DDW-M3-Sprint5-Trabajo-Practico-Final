748e891fb94a55d39330b2acacbdbe40a2b9dbb6
MAFERCARRE <mafercarrenio@gmail.com>
2026-05-23 23:57:59 -0300

    Agregué contenido pagina acerca-de

--- src/config/dbConfig.mjs
+++ src/config/dbConfig.mjs
@@ -7,7 +7,7 @@ import mongoose from 'mongoose';
 
 export async function connectDB(){
     try {    
-
+        // Conexión a MongoDB Atlas usando la URI de conexión
         await mongoose.connect('mongodb+srv://grupo-04:grupo-04@cluster0.blryo.mongodb.net/NodeMod3Cohorte5');
         console.log('Conexión exitosa a MongoDB');
         
